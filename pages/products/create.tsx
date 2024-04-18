24b7b01b92e9b5f66e96854b1e489f90308a2def
Tie Antono <tietieantono@gmail.com>
2024-04-18 17:57:54 +0700

    Added Zod validation example on Create Product form

--- pages/products/create.tsx
+++ pages/products/create.tsx
@@ -3,6 +3,8 @@ import { Page } from "@/types/Page"
 import { Button, Col, Input, InputNumber, Row, Space } from "antd";
 import React, { FormEvent, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
 
 /**
  * Define the create form data structure.
@@ -18,15 +20,99 @@ interface CreateProductFormData {
  */
 const CreateProductPage: Page = () => {
     return <>
-        {CreateForm({})}
+        {CreateProductForm({})}
     </>
 }
 
 /**
- * Create product form component using React Hook Form and Zod validation.
+ * Create the create product form Zod schema.
+ */
+const CreateProductFormSchema = z.object({
+    name: z.string().nonempty({message: 'Product name is required.'})
+    .max(50, {message: 'Product name must be less than 50 characters.'}),
+    price: z.number().min(10_000, {message: 'Product price must be at least 10,000.'})
+});
+
+/**
+ * Obtain the typing from the schema.
+ */
+type CreateProductFormType = z.infer<typeof CreateProductFormSchema>;
+
+/**
+ * Create product form component using React Hook Form & Zod validation.
  * @returns 
  */
-const CreateForm: React.FC = () => {
+const CreateProductForm: React.FC = () => {
+    const { handleSubmit, control, formState: { errors } } = useForm<CreateProductFormType>({
+        resolver: zodResolver(CreateProductFormSchema),
+    });
+
+    /**
+     * Handle the form submission event.
+     * @param e 
+     * @returns 
+     */
+    function onFormSubmit(formData: CreateProductFormData) {
+        alert(`Product Name: ${formData.name}\nProduct Price: ${formData.price}`);
+    }
+
+    return <Space direction="vertical" size={"middle"} style={{ display: 'flex' }}>
+        <Row>
+            <Col span={24}>
+                <h1>Create Product</h1>
+                <p>Fill in the form below to create a new product.</p>
+            </Col>
+        </Row>
+
+        <Row>
+            <Col span={24}>
+                <form onSubmit={handleSubmit(onFormSubmit)}>
+                    <Space direction="vertical" size={"small"} style={{ display: 'flex' }}>
+                        <Row>
+                            <Col span={24}>
+                                {/* When using controlled inputs such as antd's Input component, 
+                                we must use the Controller component */}
+                                <Controller name="name"
+                                    control={control}
+                                    render={({ field }) => <Input placeholder="Product Name"
+                                        addonBefore="Product Name" {...field} />} />
+                                {errors.name && <span className="text-red-500">{errors.name.message}</span>}
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col span={18}>
+                                <Controller name="price"
+                                    control={control}
+                                    render={({ field }) => <InputNumber defaultValue={0}
+                                        addonBefore="Product Price" {...field} />} />
+
+                                {errors.price && <span className="text-red-500">{errors.price.message}</span>}
+                            </Col>
+                        </Row>
+
+                        <Button type="primary" htmlType="submit" className="bg-blue-500">Submit</Button>
+                    </Space>
+                </form>
+            </Col>
+        </Row>
+
+        <Row>
+            {/* <Col span={24}>
+                <h2>Input Summary</h2>
+                <p>Product Name: {formData.name}</p>
+                <p>Product Price: {formData.price}</p>
+            </Col> */}
+        </Row>
+    </Space>
+}
+
+
+/**
+ * Create product form component using React Hook Form validation.
+ * @returns 
+ */
+export const CreateForm: React.FC = () => {
     const { handleSubmit, control, formState: { errors } } = useForm<CreateProductFormData>();
 
     /**
